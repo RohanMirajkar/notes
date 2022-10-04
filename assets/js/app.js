de47b7960e90bb429e9018c0df882e2e9326ddc3
Rohan Mirajkare <rohanmirajkar3008@gmail.com>
2022-10-05 02:30:17 +0530

    worked on UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,21 +4,34 @@ const myText = document.getElementById('textArea');
 const noNote = document.getElementById('noNote')
 const DeleteNote = document.getElementById('DeleteNote');
 const noteSection = document.getElementById('notes');
-let myprevent ;
+let weeks =[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+let d = new Date;
+let day = d.getDay();
+let date = d.getDate()
+let hr = d.getHours();
+let min = d.getMinutes();
+let month = d.getMonth();
+let year = d.getFullYear();
 
 
-
-let myNotesData =[];
 let dataBase =[];
-let deletenote =[];
 
 const templating = (arr)=>{
     result ='';
+    
     arr.forEach((note , i) =>{
     result += `
     <div class="col-sm-6 col-md-4 mb-3">
                 <div class="card p-3 h-100">
-                    <h5 class="h5">note ${i + 1}</h5>
+                    <h5 class="h5">note ${i + 1}<small>created on ${weeks[day]} at ${hr} : ${min} day -${date} / ${month} / ${year}</small></h5>
                     <div class="card-body p-0">
                         <p class="p-0">${note.noteText}</p>
                     </div>
